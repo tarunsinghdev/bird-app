e43392c23bc8d146a612619573e2d44f98377c7d
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-07-17 22:29:34 +0530

    implement Authentication

---
+++ server/routes/authRoute.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { signup, signin, signout } from '../controllers/authController.js';
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/signin', signin);
+router.get('/signout', signout);
+
+export default router;
