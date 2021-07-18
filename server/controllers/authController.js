694788e1d19a14f54e26daad921e12652f4890d6
Yogesh Yadav <2803yogesh.yadav@gmail.com>
2021-07-18 19:33:39 +0530

    Refactor auth logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,28 +1,22 @@
 import User from '../models/userModal.js';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import { validationResult } from 'express-validator';
-const saltRounds = 10;
 
 export const signup = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
-
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    req.body.password = hash;
-    const user = new User(req.body);
-    user.save((err, user) => {
-      if (err) {
-        return res.status(400).json({
-          error: 'Not able to save user in DB',
-        });
-      }
-      res.json({
-        firstName: user.firstName,
-        id: user._id,
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Not able to save user in DB',
       });
+    }
+    res.json({
+      firstName: user.firstName,
+      id: user._id,
     });
   });
 };
@@ -41,26 +35,23 @@ export const signin = (req, res) => {
       });
     }
 
-    bcrypt.compare(password, user.password, function (err, result) {
-      if (result == false) {
-        return res
-          .status(401)
-          .json({ error: "Email and password doesn't match" });
-      }
-      if (result == true) {
-        //create token using user's id
-        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    if (!user.matchPassword(password)) {
+      return res
+        .status(401)
+        .json({ error: "Email and password doesn't match" });
+    }
 
-        //store token in browser cookie
-        res.cookie('token', token, { expire: new Date() + 9999 });
+    //create token using user's id
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
 
-        //sending response to frontend
-        const { _id, firstName, lastName, username, email } = user;
-        res.json({
-          token,
-          user: { _id, firstName, lastName, username, email },
-        });
-      }
+    //store token in browser cookie
+    res.cookie('token', token, { expire: new Date() + 9999 });
+
+    //sending response to frontend
+    const { _id, firstName, lastName, username, email } = user;
+    res.json({
+      token,
+      user: { _id, firstName, lastName, username, email },
     });
   });
 };
